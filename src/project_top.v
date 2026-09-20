e0d18c42f389e71bf16ce770cf001ebd178cd184
fouimet <felix.ouimet@gmail.com>
2026-09-20 14:47:51 -0400

    structure and info updates

--- src/project_top.v
+++ src/project_top.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Felix Ouimet
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_fouimet (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -23,5 +23,7 @@ module tt_um_example (
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+
+  //States
 
 endmodule
